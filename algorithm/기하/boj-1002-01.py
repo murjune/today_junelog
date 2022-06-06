5af1324c2e336783e0b6e36aea5929ca3f2b8c9a
JUNWON LEE <87055456+murjune@users.noreply.github.com>
2022-06-06 21:35:30 +0900

    [FEAT] BOJ-터렛

--- algorithm/기하/boj-1002-01.py
+++ algorithm/기하/boj-1002-01.py
@@ -1,13 +1,23 @@
+# https://www.acmicpc.net/problem/1002
 
 for _ in range(int(input())):
     x1,y1, r1, x2,y2,r2 = map(int,input().split())
 
     dist = (x1-x2)**2 + (y1-y2)**2
-    pivot = (r1+r2)**2
+    pivot1 = (r1+r2)**2
+    pivot2 = (r1-r2)**2
 
-    if pivot == dist:
-        print(1)
-    elif pivot > dist:
+    if pivot2 < dist < pivot1:
         print(2)
-    else:
+    elif dist == pivot1:
+        print(1)
+    elif dist > pivot1:
         print(0)
+    elif pivot2 > dist:
+        print(0)
+    elif pivot2 == dist:
+        if (x1,y1) == (x2, y2):
+            print(-1)
+        else:
+            print(1)
+
